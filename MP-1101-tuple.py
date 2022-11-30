54abd15e654199f7c08836949f6df5bfa422e784
MP <phonlakrit.tiraratn@gmail.com>
2022-11-30 08:11:52 +0700

    Accept any test case.

--- MP-1101-tuple.py
+++ MP-1101-tuple.py
@@ -1,8 +1,11 @@
+def make_tuple(space_seperated):
+    return tuple(space_seperated.split(" "))
+
 def neglect_two(base_tuple):
     return tuple(base_tuple[i] for i in range(len(base_tuple)) if i != 1)
 
 def main():
-    base_tuple = ("a", 1, "b", 2)
+    base_tuple = make_tuple(input("Enter elements seperated by space: "))
     new_tuple = neglect_two(base_tuple=base_tuple)
     print(f"Base tuple: {base_tuple} \nProcessed tuple: {new_tuple}")
 
