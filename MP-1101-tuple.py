197bbbf534fc74279d9097beae88b01541d14d5c
MP <phonlakrit.tiraratn@gmail.com>
2022-12-02 07:53:03 +0700

    I read it wrong lol

--- MP-1101-tuple.py
+++ MP-1101-tuple.py
@@ -1,12 +1,35 @@
-def make_tuple(space_seperated):
-    return tuple(space_seperated.split(" "))
+"""
+function generate_tuple
+    generate 5 random integer
+    append each into the list
+    return list converted into tuple
 
-def neglect_two(base_tuple):
-    return tuple(base_tuple[i] for i in range(len(base_tuple)) if i != 1)
+function negelct second
+    keep element such that its index divided by 2 is not equal to 0
+    return value at odd element
+"""
 
+from random import randint
+
+def generate_tuple(length):
+    """
+    :param length: int (length of tuple)
+    :return: tuple of random element of the specified length
+    """
+    base_lst = []
+    for element_order in range(length):
+        base_lst.append(randint(0, 20))
+    return tuple(base_lst)
+
+def neglect_even_element(base_tuple):
+    """
+    :param base_tuple: tuple (tuple to be processed)
+    :return: tuple with second element removed
+    """
+    return tuple(base_tuple[i] for i in range(len(base_tuple)) if i % 2 == 0)
 def main():
-    base_tuple = make_tuple(input("Enter elements seperated by space: "))
-    new_tuple = neglect_two(base_tuple=base_tuple)
+    base_tuple = generate_tuple(int(input("Specify the tuple length: ")))
+    new_tuple = neglect_even_element(base_tuple=base_tuple)
     print(f"Base tuple: {base_tuple} \nProcessed tuple: {new_tuple}")
 
 if __name__ == "__main__":
